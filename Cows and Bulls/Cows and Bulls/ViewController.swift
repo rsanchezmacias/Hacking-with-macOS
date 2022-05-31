f18f8272a6b0f838f8882c9c9b33690111aa2747
Ricardo Sanchez <r.sanchez.macias.dev@gmail.com>
2022-05-30 20:11:51 -0400

    First commit

---
+++ Cows and Bulls/Cows and Bulls/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cows and Bulls
+//
+//  Created by Ricardo Sanchez-Macias on 5/29/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+## Hacking with macOS, a Udemy course
+
+This repository is a collection of projects done as part of *Hacking with macOS* 
+
+# Hacking-with-macOS
